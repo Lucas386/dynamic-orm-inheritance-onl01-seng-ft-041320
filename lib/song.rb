52bcc210e6f723e499ab8629ddb8529f84c7953e
Lucas Easley <lucaseasley11@gmail.com>
2020-05-14 19:29:11 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,15 @@
-require_relative "./interactive_record.rb"
 
-class Song < InteractiveRecord
+require_relative "../config/environment.rb"
+require 'active_support/inflector'
+require 'interactive_record.rb'
 
-  self.column_names.each do |col_name|
-    attr_accessor col_name.to_sym
-  end
+class Student < InteractiveRecord
+
+
+    self.column_names.each do |column_name|
+      #turn each column name into a symbol with .to_sym
+      attr_accessor column_name.to_sym
+    end
+
 
 end
